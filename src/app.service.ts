52fb681cfb29ce8c526f87f94b025f36e2b4e885
fl0k13 <antworker1337@gmail.com>
2024-04-14 21:37:01 +0400

    added subscribe checking

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,27 @@ import { Context } from 'telegraf';
 @Injectable()
 export class AppService {
   async startCommand(ctx: Context) {
-    await ctx.replyWithHTML(process.env.START_TEXT);
+    const chatId = 1001896619302;
+    const userId = ctx.from.id;
+
+    try {
+      const chatMember = await ctx.telegram.getChatMember(chatId, userId);
+
+      if (
+        chatMember.status === 'member' ||
+        chatMember.status === 'administrator'
+      ) {
+        return await ctx.replyWithHTML(
+          `Ты подписан на секретный канал с доступом`,
+        );
+      } else {
+        return await ctx.replyWithHTML(process.env.START_TEXT);
+      }
+    } catch (error) {
+      console.log(error);
+      return await ctx.replyWithHTML(
+        `Произошла ошибка при проверке подписки на канал`,
+      );
+    }
   }
 }
