e12590eadf229339e817bfb57e1607ec516e593c
fl0k13 <antworker1337@gmail.com>
2024-04-16 00:42:07 +0400

    fix

--- src/scenes/start.scene.ts
+++ src/scenes/start.scene.ts
@@ -4,34 +4,38 @@ import { SceneContext } from 'telegraf/scenes';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
 import { Repository } from 'typeorm';
+import { Markup } from 'telegraf';
 
 @Injectable()
 @Scene('startScene')
 export class StartScene {
+  private MESSAGE_ID: number;
   constructor(
     @InjectRepository(User) private readonly userEntity: Repository<User>,
   ) {}
   @SceneEnter()
   async startEnter(@Ctx() ctx: SceneContext) {
-    const keyboard = await ctx.reply(`Привет, ${ctx.from.first_name}`, {
-      reply_markup: {
-        inline_keyboard: [
-          [{ text: '📜Аккаунты', callback_data: 'logs' }],
-          [{ text: '📂Мой профиль', callback_data: 'profile' }],
-          [{ text: '❔Поддержка', callback_data: 'support' }],
-        ],
-      },
-    });
+    const message = await ctx.replyWithHTML(
+      `<b>Привет, ${ctx.from.first_name}</b>`,
+    );
+    this.MESSAGE_ID = message.message_id;
+    return Markup.inlineKeyboard([
+      Markup.button.callback('📩Аккаунты', 'logs'),
+      Markup.button.callback('📔Мой профиль', 'profile'),
+      Markup.button.callback('❔Поддержка', 'support'),
+    ]);
   }
 
   @Action('logs')
   async startLogsScene(@Ctx() ctx: SceneContext) {
+    await ctx.deleteMessage(this.MESSAGE_ID);
     await ctx.scene.enter('logScene');
   }
 
   @Action('profile')
   async profile(@Ctx() ctx: SceneContext) {
     const name = ctx.from.first_name;
+    await ctx.deleteMessage(this.MESSAGE_ID);
     const user = await this.userEntity.findOne({
       where: { id: ctx.from.id },
     });
@@ -46,6 +50,7 @@ export class StartScene {
 
   @Action('support')
   async support(@Ctx() ctx: SceneContext) {
+    await ctx.deleteMessage(this.MESSAGE_ID);
     await ctx.replyWithContact('79020410729', 'Fl0k13');
     await ctx.editMessageReplyMarkup({
       inline_keyboard: [[{ text: '◀️', callback_data: 'backMenu' }]],
